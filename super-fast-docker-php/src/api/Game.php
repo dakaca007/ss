97324b60066ff7a652cac8e54e73ad0301764397
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2025-06-17 10:43:52 +0000

    a

--- super-fast-docker-php/src/api/Game.php
+++ super-fast-docker-php/src/api/Game.php
@@ -37,11 +37,13 @@ class GameApi {
                 return ['code' => 404, 'msg' => '接口不存在', 'info' => []];
         }
     }
-
     public function settleGame($params, $domain) {
         $uid = checkNull($params['uid'] ?? '');
         $gameid = checkNull($params['gameid'] ?? '');
         $info = $domain->settleGame($uid, $gameid);
+        if ($info === 1000) {
+            return ['code' => 1000, 'msg' => '游戏信息不存在', 'info' => []];
+        }
         return ['code' => 0, 'msg' => '', 'info' => [$info]];
     }
     public function checkGame($params, $domain) {
@@ -54,15 +56,30 @@ class GameApi {
         $liveuid = checkNull($params['liveuid'] ?? '');
         $stream = checkNull($params['stream'] ?? '');
         $token = checkNull($params['token'] ?? '');
+        if ($liveuid < 1 || $token == '' || $stream == '') {
+            return ['code' => 1001, 'msg' => '信息错误', 'info' => []];
+        }
+        $checkToken = checkToken($liveuid, $token);
+        if ($checkToken == 700) {
+            return ['code' => 700, 'msg' => '您的登陆状态失效，请重新登陆！', 'info' => []];
+        }
         $info = $domain->Jinhua($liveuid, $stream, $token);
         return ['code' => 0, 'msg' => '', 'info' => [$info]];
     }
     public function endGame($params, $domain) {
         $liveuid = checkNull($params['liveuid'] ?? '');
         $gameid = checkNull($params['gameid'] ?? '');
+        $token = checkNull($params['token'] ?? '');
         $type = checkNull($params['type'] ?? '');
         $ifset = checkNull($params['ifset'] ?? '');
+        $checkToken = checkToken($liveuid, $token);
+        if ($checkToken == 700) {
+            return ['code' => 700, 'msg' => '您的登陆状态失效，请重新登陆！', 'info' => []];
+        }
         $info = $domain->endGame($liveuid, $gameid, $type, $ifset);
+        if ($info === 1000) {
+            return ['code' => 1000, 'msg' => '该游戏已经被关闭', 'info' => []];
+        }
         return ['code' => 0, 'msg' => '', 'info' => [$info]];
     }
     public function JinhuaBet($params, $domain) {
@@ -71,21 +88,46 @@ class GameApi {
         $token = checkNull($params['token'] ?? '');
         $coin = checkNull($params['coin'] ?? '');
         $grade = checkNull($params['grade'] ?? '');
+        $checkToken = checkToken($uid, $token);
+        if ($checkToken == 700) {
+            return ['code' => 700, 'msg' => '您的登陆状态失效，请重新登陆！', 'info' => []];
+        }
         $info = $domain->JinhuaBet($uid, $gameid, $token, $coin, $grade);
+        if ($info === 1000) {
+            return ['code' => 1000, 'msg' => '你的余额不足，无法下注', 'info' => []];
+        } elseif ($info === 1001) {
+            return ['code' => 1001, 'msg' => '本轮游戏已经结束', 'info' => []];
+        } elseif ($info === 1002) {
+            return ['code' => 1002, 'msg' => '下注失败', 'info' => []];
+        } elseif ($info === 1003) {
+            return ['code' => 1003, 'msg' => '下注金额已达上限', 'info' => []];
+        }
         return ['code' => 0, 'msg' => '', 'info' => [$info]];
     }
     public function Dial($params, $domain) {
         $liveuid = checkNull($params['liveuid'] ?? '');
         $stream = checkNull($params['stream'] ?? '');
         $token = checkNull($params['token'] ?? '');
+        if ($liveuid < 1 || $token == '' || $stream == '') {
+            return ['code' => 1001, 'msg' => '信息错误', 'info' => []];
+        }
+        $checkToken = checkToken($liveuid, $token);
+        if ($checkToken == 700) {
+            return ['code' => 700, 'msg' => '您的登陆状态失效，请重新登陆！', 'info' => []];
+        }
         $info = $domain->Dial($liveuid, $stream, $token);
         return ['code' => 0, 'msg' => '', 'info' => [$info]];
     }
     public function Dial_end($params, $domain) {
         $liveuid = checkNull($params['liveuid'] ?? '');
         $gameid = checkNull($params['gameid'] ?? '');
+        $token = checkNull($params['token'] ?? '');
         $type = checkNull($params['type'] ?? '');
         $ifset = checkNull($params['ifset'] ?? '');
+        $checkToken = checkToken($liveuid, $token);
+        if ($checkToken == 700) {
+            return ['code' => 700, 'msg' => '您的登陆状态失效，请重新登陆！', 'info' => []];
+        }
         $info = $domain->Dial_end($liveuid, $gameid, $type, $ifset);
         return ['code' => 0, 'msg' => '', 'info' => [$info]];
     }
@@ -95,7 +137,20 @@ class GameApi {
         $token = checkNull($params['token'] ?? '');
         $coin = checkNull($params['coin'] ?? '');
         $grade = checkNull($params['grade'] ?? '');
+        $checkToken = checkToken($uid, $token);
+        if ($checkToken == 700) {
+            return ['code' => 700, 'msg' => '您的登陆状态失效，请重新登陆！', 'info' => []];
+        }
         $info = $domain->Dial_Bet($uid, $gameid, $token, $coin, $grade);
+        if ($info === 1000) {
+            return ['code' => 1000, 'msg' => '你的余额不足，无法下注', 'info' => []];
+        } elseif ($info === 1001) {
+            return ['code' => 1001, 'msg' => '本轮游戏已经结束', 'info' => []];
+        } elseif ($info === 1002) {
+            return ['code' => 1002, 'msg' => '下注失败', 'info' => []];
+        } elseif ($info === 1003) {
+            return ['code' => 1003, 'msg' => '下注金额已达上限', 'info' => []];
+        }
         return ['code' => 0, 'msg' => '', 'info' => [$info]];
     }
     public function getGameRecord($params, $domain) {
@@ -121,12 +176,17 @@ class GameApi {
         $token = checkNull($params['token'] ?? '');
         $stream = checkNull($params['stream'] ?? '');
         $deposit = checkNull($params['deposit'] ?? '');
-        $info = $domain->setBanker($uid);
-        return ['code' => 0, 'msg' => '', 'info' => [$info]];
+        $checkToken = checkToken($uid, $token);
+        if ($checkToken == 700) {
+            return ['code' => 700, 'msg' => '您的登陆状态失效，请重新登陆！', 'info' => []];
+        }
+        $info = $domain->setBanker($uid, $deposit, $stream);
+        return $info;
     }
     public function quietBanker($params, $domain) {
         $uid = checkNull($params['uid'] ?? '');
         $stream = checkNull($params['stream'] ?? '');
-        return ['code' => 0, 'msg' => '', 'info' => [['msg' => '下庄成功']]];
+        $info = $domain->quietBanker($uid, $stream);
+        return $info;
     }
 }
